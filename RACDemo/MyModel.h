d3486f13e045aa24a7ef35389862abf88db734d9
Mario Stallone <mariostallone@gmail.com>
2013-07-08 11:23:09 +0530

    Model added

---
+++ RACDemo/MyModel.h
@@ -0,0 +1,18 @@
+//
+//  MyModel.h
+//  RACDemo
+//
+//  Created by Mario on 08/07/13.
+//  Copyright (c) 2013 Mario. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyModel : NSObject
+
+@property (nonatomic,retain) NSString *name;
+@property (nonatomic,retain) NSString *username;
+@property (nonatomic,retain) NSString *email;
+@property (nonatomic,retain) NSString *github;
+
+@end
---
+++ RACDemo/MyModel.m
@@ -0,0 +1,13 @@
+//
+//  MyModel.m
+//  RACDemo
+//
+//  Created by Mario on 08/07/13.
+//  Copyright (c) 2013 Mario. All rights reserved.
+//
+
+#import "MyModel.h"
+
+@implementation MyModel
+
+@end
